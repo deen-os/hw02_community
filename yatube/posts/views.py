cbe61448fc2f500d2c0ea6a0464ab16771d58a3d
Dimitri Larios <lariosdimitri@yandex.ru>
2022-01-10 17:34:56 +0300

    second review passed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,7 +17,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:NUMBER_OF_POSTS]
+    posts = group.posts.all()[:NUMBER_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
